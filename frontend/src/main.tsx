512e8a8eab3f17dd3c82ec5241905f3e48079670
lyeskara <lyeskara1106@gmail.com>
2023-07-14 23:27:46 -0400

    wrapped the app with the jotai state provider for global state

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './globals.css'
-
+import { BrowserRouter } from 'react-router-dom'
+import { Provider as JotaiProvider } from 'jotai'
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <JotaiProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </JotaiProvider>
   </React.StrictMode>,
 )
