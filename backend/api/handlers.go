b9f4a4c6005effdfbba908d85492ee3547a5ccb8
lyeskara <lyeskara1106@gmail.com>
2023-07-14 23:22:07 -0400

    completed the signup controller

--- backend/api/handlers.go
+++ backend/api/handlers.go
@@ -3,6 +3,7 @@ package api
 import (
 	"backend/configs"
 	"backend/models"
+	"backend/queries"
 	"encoding/json"
 	"log"
 	"net/http"
@@ -19,16 +20,30 @@ func Signup(w http.ResponseWriter, r *http.Request) {
 	}
 	password := configs.GeneratePassword()
 	user.Password, err = configs.HashPassword(password)
-	if err != nil{
+	if err != nil {
 		log.Fatal(err)
 	}
+	
+	Existance, err := queries.UserExist(db, user)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if Existance {
+		w.WriteHeader(http.StatusConflict)
+		w.Write([]byte("Username already exists"))
+		return
+	}
+
 	_, err = db.Exec("INSERT INTO users (username, password) VALUES ($1, $2)", user.Username, user.Password)
 	if err != nil {
 		log.Fatal(err)
 	}
-
+	JWToken, err := configs.GenerateJWToken(&user)
+	if err != nil {
+		log.Fatal(err, 1)
+	}
+	w.Header().Set("Authorization", "Bearer "+JWToken)
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(map[string]string{"message": "User created successfully"})
 
 }
 
@@ -41,9 +56,7 @@ func Login(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		log.Fatal(err)
 	}
-	
-	
-	
+
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(map[string]string{"message": "User created successfully"})
 
