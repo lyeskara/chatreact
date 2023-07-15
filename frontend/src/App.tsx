d947d376efed0928111718a91b411295137a362c
lyeskara <lyeskara1106@gmail.com>
2023-07-14 23:27:15 -0400

    added routes for different pages as well as authorization middleware

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,26 @@
 
+import Callroom from '../pages/Callroom'
+import Login from '../pages/Login'
+import Signup from '../pages/Signup'
+import SDPs from '../pages/SDPs'
+import { Route, Routes } from 'react-router-dom'
 import Nav from '../components/Nav'
+import { AuthMiddleware } from './Middleware'
+
+
 function App() {
+
 
   return (
     <>
-     <Nav/>
-     </>
+      <Nav />
+      <Routes>
+        <Route path='/callroom' element={<AuthMiddleware><Callroom /></AuthMiddleware>} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/available-rooms' element={<AuthMiddleware><SDPs /></AuthMiddleware>} />
+      </Routes>
+    </>
   )
 }
 
