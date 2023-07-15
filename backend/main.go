5b615d373edc18811cbde540ce66bbeee2eec96c
lyeskara <lyeskara1106@gmail.com>
2023-07-14 23:25:02 -0400

    made the chi router use the jwt middleware for upcoming requests

--- backend/main.go
+++ backend/main.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"backend/api"
+	"backend/middlewares"
 	"net/http"
-
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/go-chi/cors"
@@ -12,11 +12,12 @@ import (
 func main() {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
+	r.Use(middlewares.JWTmiddleware)
 	r.Use(cors.Handler(cors.Options{
-		AllowedOrigins:   []string{"http://localhost:3000"},
+		AllowedOrigins:   []string{"http://localhost:5173"},
 		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowedHeaders:   []string{"Authorization", "Content-Type"},
-		ExposedHeaders:   []string{"Link"},
+		ExposedHeaders:   []string{"Authorization","Link"},
 		AllowCredentials: false,
 		MaxAge:           300, // Maximum value not ignored by any of major browsers
 	}))
