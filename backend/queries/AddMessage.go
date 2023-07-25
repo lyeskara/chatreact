963226c21fd1363c25b7d1603827b4f64c216e51
lyeskara <lyeskara1106@gmail.com>
2023-07-24 21:16:25 -0400

    insert messages to database

---
+++ backend/queries/AddMessage.go
@@ -0,0 +1,19 @@
+package queries
+
+import (
+	"backend/models"
+	"database/sql"
+)
+
+func AddMessage(db *sql.DB, message *models.Message) error {
+	_, err := db.Exec(
+		`INSERT INTO messages (messageText, roomId, userId) 
+	        VALUES ($1, 
+			(SELECT roomId FROM rooms WHERE roomName = $2),
+			(SELECT id FROM users WHERE username = $3)) 
+        RETURNING messageId`, message.Message, message.RoomName, message.Username)
+	if err != nil {
+		return err
+	}
+	return nil
+}
