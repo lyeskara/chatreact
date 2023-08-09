821fc4ecbc0fa93160f394ff32a1172480010a8a
lyeskara <lyeskara1106@gmail.com>
2023-08-09 14:08:31 -0400

    fixed the middlewares for auth pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,20 @@
 
-import Login from '../pages/Login'
-import Signup from '../pages/Signup'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
 import { Route, Routes } from 'react-router-dom'
-import Nav from '../components/Nav'
-import { AuthMiddleware } from './Middleware'
-import  Messanger from '../pages/Messenger'
-import ChatRoom from '../pages/ChatRoom'
+import { AuthMiddleware } from '../utils/authorizationMiddleware'
+import { LoginMiddleware } from '../utils/loginRedirectMiddleware'
+import Rooms from './pages/Rooms'
+import ChatRoom from './pages/ChatRoom'
 function App() {
 
 
   return (
     <>
-      <Nav />
       <Routes>
-        <Route path='/' element={<AuthMiddleware><Messanger /></AuthMiddleware>} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/signup' element={<Signup />} />
+        <Route path='/' element={<AuthMiddleware><Rooms /></AuthMiddleware>} />
+        <Route path='/login' element={<LoginMiddleware><Login /></LoginMiddleware>} />
+        <Route path='/signup' element={<LoginMiddleware><Signup /></LoginMiddleware>} />
         <Route path='/:room' element={<AuthMiddleware><ChatRoom /></AuthMiddleware>} />
       </Routes>
     </>
